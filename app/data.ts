8e25fa12ce90828cb0821908e2ee6f635a4c6c18
Niels Steenbergen <niels.steenbergen@triply.cc>
2025-11-18 13:57:36 +0100

    Fix SPARQL query.
    - `dcat:Catalog`, not `dcat:Catalogue`
    - Use the resources config to decide which resources to include beyond
      Catalog/Dataset/Distribution.

--- app/data.ts
+++ app/data.ts
@@ -17,38 +17,50 @@ class ArrayQuadStream extends Readable implements RDF.Stream<RDF.Quad> {
 }
 
 export async function queryDatabase(): Promise<RDF.Stream<RDF.Quad>> {
-  // Alternative approach is to use `query()` with the `Accept` header set to `text/turtle`,
-  // and then use `rdf-parse`, but that would require patching `mu-javascript-template`, cf.
+  // Alternative approach is to directly use `query()` (with the `Accept` header set to the appropriate MIME type
+  // (like `text/turtle`), but that would require patching `mu-javascript-template`, cf.
   // <https://github.com/mu-semtech/mu-javascript-template/blob/e7a270549a8fcdfaf0c24235751a9df8f9e40672/helpers/mu/sparql.js#L15>
+
+  // The query below returns all properties on `dcat:Catalog`s, `Dataset`s and
+  // `Distribution`s, as well as any resources also linked to them in the `resources`
+  // config of the app-decide project, cf.
+  // <https://github.com/lblod/app-decide/blob/development/config/resources/dcat.lisp>
   const sparqlJsonResponse = await query(
     `
     PREFIX cms: <http://mu.semte.ch/vocabulary/cms/>
     PREFIX dct: <http://purl.org/dc/terms/>
     PREFIX dcat: <http://www.w3.org/ns/dcat#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     CONSTRUCT {
       ?subject ?predicate ?object.
     } WHERE {
       {
-        ?subject a dcat:Catalogue.
+        ?subject a dcat:Catalog.
       } UNION {
         ?subject a dcat:Dataset.
       } UNION {
         ?subject a dcat:Distribution.
       } UNION {
-        [] a dcat:Catalogue;
+        [] a dcat:Catalog;
           dct:publisher|
-          dcat:themeTaxonomy|
-          dcat:record|
-          dcat:dataset|
-          dcat:theme|
-          dcat:record ?subject.
+          (dcat:record/foaf:primaryTopic?)|
+          dct:themeTaxonomy
+            ?subject.
+      } UNION {
+        [] a dcat:Dataset;
+          dct:publisher|
+          (dcat:theme/skos:inScheme?)
+            ?subject.
       } UNION {
         [] a dcat:Distribution;
-          dct:format/cms:page? ?subject.
+          dct:format/cms:page?
+            ?subject.
       }
       ?subject ?predicate ?object.
     }
-  `);
+  `
+  );
 
   // Since we just get a single response, we parse everything into an array of quads and make a stream out of it after the fact.
   const parser = new SparqlJsonParser();
